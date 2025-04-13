60a941ca8572fc8e349356af0d92407cb37a107b
HEMANTH PRASAD <hemanthprasad46@gmail.com>
2025-04-13 19:10:27 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <CardContent>
    
    with the text content:
    
        Information on Fundamental Rights will be added here.
    
    Relevant files:
    
        - https://6000-idx-studio-1744569238946.cluster-isls3qj2gbd5qs4jkjqvhahfv6.cloudworkstations.dev/_next/static/chunks/src_d6c284eb._.js
        - https://6000-idx-studio-1744569238946.cluster-isls3qj2gbd5qs4jkjqvhahfv6.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
        - https://6000-idx-studio-1744569238946.cluster-isls3qj2gbd5qs4jkjqvhahfv6.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
    
    And change it as follows:
    
    add fundamental rights of indian citizen

--- src/app/information-repository/fundamental-rights/page.tsx
+++ src/app/information-repository/fundamental-rights/page.tsx
@@ -12,7 +12,29 @@ const FundamentalRightsPage: React.FC = () => {
           <CardDescription>Learn about the fundamental rights guaranteed by the Indian Constitution.</CardDescription>
         </CardHeader>
         <CardContent>
-          <p>Information on Fundamental Rights will be added here.</p>
+          <p>
+            The Constitution of India guarantees six fundamental rights to its citizens:
+          </p>
+          <ol className="list-decimal list-inside pl-4">
+            <li>
+              <b>Right to Equality</b>: Ensures equality before the law and prohibits discrimination on grounds of religion, race, caste, sex, or place of birth. (Articles 14-18)
+            </li>
+            <li>
+              <b>Right to Freedom</b>: Includes freedom of speech and expression, assembly, association, movement, and the right to practice any profession or occupation. (Articles 19-22)
+            </li>
+            <li>
+              <b>Right against Exploitation</b>: Prohibits forced labor, human trafficking, and employment of children below 14 years in hazardous jobs. (Articles 23-24)
+            </li>
+            <li>
+              <b>Right to Freedom of Religion</b>: Guarantees religious freedom to all citizens, allowing them to profess, practice, and propagate their religion. (Articles 25-28)
+            </li>
+            <li>
+              <b>Cultural and Educational Rights</b>: Protects the rights of minorities to conserve their culture, language, and script, and to establish and administer educational institutions of their choice. (Articles 29-30)
+            </li>
+            <li>
+              <b>Right to Constitutional Remedies</b>: Ensures the right to approach the Supreme Court for enforcement of fundamental rights. (Article 32)
+            </li>
+          </ol>
         </CardContent>
       </Card>
     </div>
