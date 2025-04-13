621bd15c268dbbadec365fd772c81d4c1f1714bb
HEMANTH PRASAD <hemanthprasad46@gmail.com>
2025-04-13 19:03:08 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > Runtime Error: Error: ./node_modules/pdf-parse/lib/pdf.js/v1.10.100/build/pdf.js:17378:10
    > Module not found: Can't resolve 'fs'
    >   17376 | function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
    >   17377 |
    > > 17378 | var fs = require('fs');
    >         |          ^^^^^^^^^^^^^
    >   17379 | var http = require('http');
    >   17380 | var https = require('https');
    >   17381 | var url = require('url');
    >
    >
    >
    > https://nextjs.org/docs/messages/module-not-found
    >
    > . Error source: Call Stack
    > 9
    >
    > Show 8 ignore-listed frame(s)
    > <unknown>
    > error: ./node_modules/pdf-parse/lib/pdf.js/v1.10.100/build/pdf.js (17378:10)

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,15 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  webpack: (config, {isServer}) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+      };
+    }
+
+    return config;
+  },
 };
 
 export default nextConfig;
