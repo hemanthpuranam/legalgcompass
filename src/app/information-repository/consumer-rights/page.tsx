e5a364f7c979f6e04057931116bdd327e974e018
HEMANTH PRASAD <hemanthprasad46@gmail.com>
2025-04-13 19:14:00 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <CardContent>
    
    with the text content:
    
        Information on Consumer Rights will be added here.
    
    Relevant files:
    
        - https://6000-idx-studio-1744569238946.cluster-isls3qj2gbd5qs4jkjqvhahfv6.cloudworkstations.dev/_next/static/chunks/src_e0b7bb97._.js
        - https://6000-idx-studio-1744569238946.cluster-isls3qj2gbd5qs4jkjqvhahfv6.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
        - https://6000-idx-studio-1744569238946.cluster-isls3qj2gbd5qs4jkjqvhahfv6.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
    
    And change it as follows:
    
    add information about consumer rights of indian citizens

--- src/app/information-repository/consumer-rights/page.tsx
+++ src/app/information-repository/consumer-rights/page.tsx
@@ -12,7 +12,32 @@ const ConsumerRightsPage: React.FC = () => {
           <CardDescription>Know your rights as a consumer under Indian law.</CardDescription>
         </CardHeader>
         <CardContent>
-          <p>Information on Consumer Rights will be added here.</p>
+          <p>
+            As a consumer in India, you are entitled to several rights under the Consumer Protection Act. These rights are designed to protect you from unfair trade practices and ensure that you receive quality goods and services. Here are some of your key consumer rights:
+          </p>
+          <ol className="list-decimal list-inside pl-4">
+            <li>
+              <b>Right to Safety</b>: You have the right to be protected against goods and services that are hazardous to your life and property.
+            </li>
+            <li>
+              <b>Right to Be Informed</b>: You have the right to be informed about the quality, quantity, potency, purity, standard, and price of goods and services.
+            </li>
+            <li>
+              <b>Right to Choose</b>: You have the right to access a variety of goods and services at competitive prices.
+            </li>
+            <li>
+              <b>Right to Be Heard</b>: You have the right to be heard and to have your interests considered at appropriate forums.
+            </li>
+            <li>
+              <b>Right to Seek Redressal</b>: You have the right to seek redressal against unfair trade practices or unscrupulous exploitation.
+            </li>
+            <li>
+              <b>Right to Consumer Education</b>: You have the right to consumer education and awareness.
+            </li>
+          </ol>
+          <p>
+            If you feel that any of these rights have been violated, you can file a complaint with the consumer forum or commission.
+          </p>
         </CardContent>
       </Card>
     </div>
